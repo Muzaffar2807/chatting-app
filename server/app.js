14fb781704b91efc7cca784d9d173cdd3824cdcf
Muzaffar Khan <mmkhan2807@gmail.com>
2025-02-21 18:43:54 +0530

    tiny app

--- server/app.js
+++ server/app.js
@@ -1,13 +1,44 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 const port = 3000;
 
 const app = express();
+const server = createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+ 
+  //socket.broadcast.emit("welcome", `welcome to server ${socket.id}`);
 
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive-message", data);
+  });
+});
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
